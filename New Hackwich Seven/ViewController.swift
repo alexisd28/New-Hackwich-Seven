d1de4e315f2219bb814c91a3f33ce75f152b74dc
Alexis Decker <deckera@hawaii.edu>
2023-10-12 13:55:32 -1000

    Initial Commit

---
+++ New Hackwich Seven/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New Hackwich Seven
+//
+//  Created by Yuki Decker on 10/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ New Hackwich SevenUITests/New_Hackwich_SevenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  New_Hackwich_SevenUITestsLaunchTests.swift
+//  New Hackwich SevenUITests
+//
+//  Created by Yuki Decker on 10/12/23.
+//
+
+import XCTest
+
+final class New_Hackwich_SevenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
